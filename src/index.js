86c348c3622154cbefff91a2125f425b6f6bdda3
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-08-18 13:29:14 -0700

    fix formatting

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function(){
           
            map[i].addEventListener("mouseover", function () {
                return tip2.style("visibility", "visible")
-                   .html(`<p><img class="tree-tip" src="./src/images/Tree_${i + 1}.png" alt="tree picture" height="150vh" ><br>${species[i]["location"]}</p>`)
+                   .html(`<p><img class="tree-tip" src="./src/images/Tree_${i + 1}.png" alt="tree picture" height="150vh" padding-bottom="20px"><br>${species[i]["location"]}</p>`)
                    .style("top", (event.pageY - 200) + "px")
                    .style("left", (event.pageX - 50) + "px");      
            })
