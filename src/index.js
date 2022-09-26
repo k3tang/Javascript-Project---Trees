f6dcf5b82cef8a68b2f205dea29f18c16ada836f
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-26 01:08:21 -0700

    add styling to tip and start scroll listener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as d3 from "d3";
 import { active, svg } from "d3";
+import scrollama from "scrollama";
 
 const data = require("/src/data/tree_history.json");
 // const species = require("/src/data/trees-species-img.json");
@@ -21,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
        let halfCircle = function (i) {
            let events = data[1]
+
            return svg.append("path")
                .attr("transform", "translate(250, 250)")
                .attr("d", d3.arc()
@@ -33,25 +35,30 @@ document.addEventListener("DOMContentLoaded", function(){
                .attr("stroke-width", "5px")
                .attr("fill", "transparent")
                .attr("class", "halfCircle")
-               .attr("id", i + 1) 
+               .attr("id", i + 1); 
+          }
 
-               .on("mouseover", function () { return tip.style("visibility", "visible")
-                   .html(`<div class="tip-text"><p class="tip-header">${events[i - 1].year}</p><p class="tip-body">${events[i - 1].event}</p><a class="tip-link" href=${events[i - 1].website} target="_blank" rel="noopener noreferrer">Learn More</a></div>`)
-                 }) 
-
-       }
+  
        
        for (let i = radius.length; i > 0; i--) {
             halfCircle(i);
         }  
 
+        for (let i = radius.length + 1; i > 1; i--) {
+         let ele = document.getElementById(i)
+         console.log(ele, "ele")
+        //     ele.onscroll =  function () { 
+        //     return tip.style("visibility", "visible")
+        // .html(`<div class="tip-text"><div class="tree-age">Methuselah is <div class="age-text">${events[i - 1].age} years old</div></div><p class="tip-header">In ${events[i - 1].year}</p><p class="tip-body">${events[i - 1].event}</p><a class="tip-link" href=${events[i - 1].website} target="_blank" rel="noopener noreferrer">Learn More</a></div>
+        // `)}
+        }
    
         //tip for tree-rings
         let tip = d3.select("div#contents")
            .append("div")
            .attr("id", "tip1");
 
- 
+        
 
 
 
