c873c636e2714f5806248d7e19ca66b24f6da770
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-28 00:55:08 -0700

    tree timeline page

--- src/index.js
+++ src/index.js
@@ -6,39 +6,51 @@ const data = require("/src/data/tree_history.json");
 
 
 document.addEventListener("DOMContentLoaded", function(){
-console.log("hello")
-    //container to hold svg elements
-    // const mainContainer = $d3
-    //     .select("#all-contents")
-    //     .append("div")
-    //     .attr("id", "mainContainer")
-    //     .attr("class", "container");
-
-    // //rings 
-    //    var svg = d3.select("div#mainContainer")
-    //        .append("svg")
-    //        .attr("preserveAspectRatio", "xMinYMin meet")
-    //        .attr("viewBox", "0 0 500 500")
-    //        .attr("class", "svg-content");
-
-           
-    //    var radius = [0, 10, 20, 40, 60, 70, 90, 110];
-
-    //event handler for the rings
+    //populating timeline events
     function callback_timelineEvent(i) { 
-        console.log(i)
-        // let events = data[event.target.id]
-
-        // document.getElementById(`event-${event + 1}`).innerHTML = `<div>${events.age}</div>`;
+     let timeGap = [10.196, 247.168, 500, 182.904, 87.436, 26.571, 28.733, 16.684, 4.325]
+        let events = data[1][i]
+        let currentEle =  document.getElementById(`event-${(i + 1)}`);
+        currentEle.innerHTML = 
+        `<div>${events.year}</div>
+        <div class="timeline-event">${events.event}</div>
+        <div><a class="timeline-link" href=${events.website} target="_blank" rel="noopener noreferrer">Learn More</a></div>`;
+        currentEle.style.paddingTop = `${timeGap[i]}px`
 
     }
        
-    console.log(Object.values(data).length, "data.length")
-    for (let i = 0; i < data.length; i++) {
-        console.log(i, "in function")
-    return callback_timelineEvent(i);
+    for (let i = 0; i < Object.values(data[1]).length; i++) {
+     callback_timelineEvent(i);
     }
         
+    //adding scroll event handler 
+    let timelineEvents = document.querySelectorAll(".timeline li")
+    console.log(window.innerHeight, "height")
+
+    function isElementInViewport(el) {
+        // console.log(el.getBoundingClientRect(), "el")
+        let rect = el.getBoundingClientRect();
+         return (
+            rect.top >= 0 &&
+            rect.left>= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+    }
+
+
+    function callbackFunc() {
+        for (let i = 0; i < timelineEvents.length; i++) {
+        if (isElementInViewport(timelineEvents[i])) {
+            timelineEvents[i].classList.add("in-view");
+        }
+        }
+    }
+
+    window.addEventListener("scroll", callbackFunc);
+
+
+
 
 
 })
